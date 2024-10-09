30dbb843e39d4c90194ea6965517742252ee92c0
Tudor Timi <tudortimi@users.noreply.github.com>
2024-10-09 21:43:21 +0200

    Add module with parameters to example

---
+++ examples/parameters/some_module.sv
@@ -0,0 +1,4 @@
+module some_module #(
+    parameter bit SOME_BIT_PARAM = 0,
+    parameter int SOME_INT_PARAM = 42);
+endmodule
--- examples/parameters/top.sv
+++ examples/parameters/top.sv
@@ -1,10 +1,12 @@
 module top;
   import vpi::*;
 
+  some_module #(.SOME_BIT_PARAM(1)) some_module_inst();
+
   initial begin
-    automatic vpiHandle top = vpi_handle_by_name("top", null);
-    $display("top = %d", top);
-    $display("  type = %s", vpi_get_str(vpiType, top));
-    $display("  name = %s", vpi_get_str(vpiName, top));
+    automatic vpiHandle some_module_inst = vpi_handle_by_name("top.some_module_inst", null);
+    $display("some_module_inst = %d", some_module_inst);
+    $display("  type = %s", vpi_get_str(vpiType, some_module_inst));
+    $display("  name = %s", vpi_get_str(vpiName, some_module_inst));
   end
 endmodule
